9006bb80b0557db102a94166266b0617302b4aa2
Le Thanh Trung <lt.trung2001@gmail.com>
2022-06-20 09:49:07 +0700

    up

--- app/src/main/java/com/ltbth/livedata/MainActivity.kt
+++ app/src/main/java/com/ltbth/livedata/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ltbth.livedata
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,8 +12,10 @@ import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity() {
     private lateinit var rcvUser: RecyclerView
     private lateinit var btnAdd: Button
+    private lateinit var btnSort: Button
     private lateinit var userViewModel: UserViewModel
     private lateinit var userAdapter: UserAdapter
+    private var ascending = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,24 +23,36 @@ class MainActivity : AppCompatActivity() {
 
         rcvUser = findViewById(R.id.rcv_user)
         btnAdd = findViewById(R.id.btn_add)
+        btnSort = findViewById(R.id.btn_sort)
 
         rcvUser.layoutManager = LinearLayoutManager(this)
         userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
         // Quan sat du lieu thay doi
-        val observer = object: Observer<List<User>> {
-            override fun onChanged(t: List<User>?) {
+        val userObserver =
+            Observer<List<User>> { t ->
                 userAdapter = UserAdapter(t?: arrayListOf())
                 rcvUser.adapter = userAdapter
             }
-        }
 
-        userViewModel.mutableListLiveData.observe(this,observer)
+        userViewModel.mutableListLiveData.observe(this,userObserver)
         // Remove lang nghe su thay doi
 //        userViewModel.mutableListLiveData.removeObserver(observer)
 
         btnAdd.setOnClickListener {
             val user = User(R.drawable.me,"Username ${userAdapter.itemCount}","Bla bla bla")
             userViewModel.addUser(user)
+        }
+
+        btnSort.setOnClickListener {
+            if (ascending) {
+                Toast.makeText(this@MainActivity,"ASCENDING",Toast.LENGTH_SHORT).show()
+                userViewModel.descending()
+                ascending = false
+            } else {
+                Toast.makeText(this@MainActivity,"DESCENDING",Toast.LENGTH_SHORT).show()
+                userViewModel.ascending()
+                ascending = true
+            }
         }
     }
 }
--- app/src/main/java/com/ltbth/livedata/UserViewModel.kt
+++ app/src/main/java/com/ltbth/livedata/UserViewModel.kt
@@ -1,13 +1,49 @@
 package com.ltbth.livedata
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import java.util.*
+import kotlin.Comparator
+import kotlin.collections.ArrayList
 
 class UserViewModel : ViewModel() {
-    var mutableListLiveData: MutableLiveData<List<User>> = MutableLiveData()
-    var users: List<User> = arrayListOf(User(R.drawable.me,"Thành Trung","Bla bla bla"))
+
+//    val usersMediatorLiveData: MediatorLiveData<List<User>> by lazy {
+//        MediatorLiveData<List<User>>()
+//    }
+    private val users: List<User> = arrayListOf(User(R.drawable.me, "Thành Trung", "Bla bla bla"))
+    val mutableListLiveData: MutableLiveData<List<User>> by lazy {
+        MutableLiveData()
+    }
+
     init {
         mutableListLiveData.value = users
+//        usersMediatorLiveData.addSource(mutableListLiveData) {
+//            usersMediatorLiveData.value = it
+//        }
+    }
+
+    fun descending() {
+        mutableListLiveData.value?.let {
+            Collections.sort(
+                users
+            ) { o1, o2 ->
+                o2.name.compareTo(o1.name)
+            }
+            mutableListLiveData.value = users
+//            usersMediatorLiveData.value = users
+        }
+    }
+
+    fun ascending() {
+        mutableListLiveData.value?.let {
+            Collections.sort(
+                users
+            ) { o1, o2 ->
+                o1.name.compareTo(o2.name)
+            }
+            mutableListLiveData.value = users
+//            usersMediatorLiveData.value = users
+        }
     }
 
     fun addUser(user: User) {
